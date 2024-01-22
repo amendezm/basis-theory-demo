83d62223171845dadf264e64ac852b29c47572e9
amendezm <alexis.david.mendez@gmail.com>
2024-01-22 15:56:56 -0500

    style: add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { CardElement, useBasisTheory } from "@basis-theory/basis-theory-react";
 
 const App = () => {
   // creates a new instance of BasisTheory class
-  const { bt, error } = useBasisTheory("<API_KEY>", { elements: true });
+  const { bt, error } = useBasisTheory("key_us_pub_RZkK7qbfy9nTKZ9m61Y6tD", {
+    elements: true,
+  });
 
   // instance stays undefined during initialization
   if (bt) {
@@ -14,9 +16,14 @@ const App = () => {
   }
 
   return (
-    <main className="w-full h-screen flex justify-center items-center">
-      <div className="w-10/12 md:w-3/5 max-w-sm rounded-lg border border-gray-100 shadow-sm p-2">
-        <CardElement id="test" bt={bt} />
+    <main className="w-full h-screen flex justify-center items-center bg-gray-900">
+      <div className="w-10/12 md:w-3/5 max-w-lg rounded-lg border bg-gray-950 border-gray-950 shadow-sm p-2 text-gray-300">
+        <CardElement
+          id="test"
+          bt={bt}
+          style={{ base: { color: "white" } }}
+          onChange={(event) => console.log(event)}
+        />
       </div>
     </main>
   );
